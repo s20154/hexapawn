eccdf0d4926b85a0ca229beb4af5bb17dc719b99
Damian Kijanczuk <kijanczukdamian@gmail.com>
2022-10-10 18:12:19 +0200

    Add functions to implement

---
+++ hexapawn.py
@@ -0,0 +1,30 @@
+"""
+Gra:
+    Hexapawn[https://pl.wikipedia.org/wiki/Hexapawn]
+
+Autorzy:
+    Damian Kijańczuk s20154
+    Szymon Ciemny 
+
+Przygotowanie środowiska:
+    Oprócz języka Python, potrzebna takze będzie biblioteka easyAI[https://zulko.github.io/easyAI/installation.html]
+
+"""
+
+
+from easyAI import TwoPlayerGame
+from easyAI import AI_Player, Human_Player, Negamax
+
+class Hexapawn(TwoPlayerGame):
+    def __init__(self, players, size=(3, 3)):
+
+    def possible_moves(self):
+
+    def make_move(self, move):
+
+    def is_over(self):
+
+    def show(self):
+
+
+if __name__ == "__main__":
